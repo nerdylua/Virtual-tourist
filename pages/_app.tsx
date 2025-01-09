e0556a7ff3abc46242c761d181c7f9bdc5f15fd8
nerdylelouch <150607136+nerdylelouch@users.noreply.github.com>
2025-01-09 22:21:47 +0530

    more changes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,7 @@
 
 import React from "react";
 import { AppProps } from "next/app";
-import "../app/globals.css"; // Import your global styles
+import "../styles/globals.css"; // Import your global styles
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
